233bead3f301091ed5daa3109c3571bc9ecc15f4
Suvesh Pagam <156836161+Suvesh0707@users.noreply.github.com>
2025-02-22 09:16:52 +0530

    fix typo of db

--- backend/src/db/index.js
+++ backend/src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const connectDb = async() =>{
     try {
-        const connection = await mongoose.connect(`${process.env.MONGODB_URL}\task-manager`)
+        const connection = await mongoose.connect(`${process.env.MONGODB_URL}/task-manager`)
         console.log('Connected to MongoDB', connection.connection.host)
         } catch (error) {
             console.error('Error connecting to MongoDB:', error)
